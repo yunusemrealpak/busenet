b3f8a9c6699df5e2673df676140c716bf7223437
Yunus Emre Alpak <48226433+yunusemrealpak@users.noreply.github.com>
2023-09-04 11:45:17 +0300

    export empty entity model

--- lib/busenet.dart
+++ lib/busenet.dart
@@ -7,6 +7,7 @@ export 'src/models/base_entity.dart';
 export 'src/models/base_response.dart';
 export 'src/models/no_result_response.dart';
 export 'src/models/empty_response_model.dart';
+export 'src/models/empty_entity.dart';
 export 'src/models/failure/failure.dart';
 export 'src/models/failure/error_messages.dart';
 
